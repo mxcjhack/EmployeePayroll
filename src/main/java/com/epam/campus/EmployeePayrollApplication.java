2df88fbb83ccc81e30d91b371c1e20a9ca9b2b5f
Prasoon Modi <prasoon_modi@epam.com>
2025-02-19 18:16:45 +0530

    Spring boot done

--- src/main/java/com/epam/campus/EmployeePayrollApplication.java
+++ src/main/java/com/epam/campus/EmployeePayrollApplication.java
@@ -1,7 +1,6 @@
 package com.epam.campus;
 
 import com.epam.campus.console.ConsoleView;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
